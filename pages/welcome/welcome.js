adfed38b90ec2398bcc8f95884e604a4a9f51c55
unkper <33563150+unkper@users.noreply.github.com>
2018-12-02 18:12:29 +0800

    制作了四个新的页面

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,5 +1,16 @@
 Page(
   {
+    loginFunc:function(event){
+      console.log("success");
+      wx.switchTab({
+        url: "../activity/activity",
+      });
+    },
 
+    registerFunc:function(event){
+      wx.redirectTo({
+        url: '../register/register',
+      })
+    }
   }
 )
